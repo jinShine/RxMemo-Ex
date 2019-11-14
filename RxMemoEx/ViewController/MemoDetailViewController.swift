0a3d378e5b2eda07755a27b48c341c309e820cb7
seungjin <seungjin429@naver.com>
2019-11-14 20:22:29 +0900

    detail scene

--- RxMemoEx/ViewController/MemoDetailViewController.swift
+++ RxMemoEx/ViewController/MemoDetailViewController.swift
@@ -12,6 +12,11 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
 
   var viewModel: MemoDetailViewModel!
 
+  @IBOutlet weak var listTableView: UITableView!
+  @IBOutlet weak var deleteButton: UIBarButtonItem!
+  @IBOutlet weak var editButton: UIBarButtonItem!
+  @IBOutlet weak var shareButton: UIBarButtonItem!
+
   override func viewDidLoad() {
       super.viewDidLoad()
 
